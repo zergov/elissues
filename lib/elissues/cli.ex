90c1dad09dfda6341acff254c879e252072840fd
Jonathan Lalande <jonathan.lalande.1@ens.etsmtl.ca>
2019-04-04 00:09:11 -0400

    add CLI + testts for it

---
+++ lib/elissues/cli.ex
@@ -0,0 +1,31 @@
+defmodule Elissues.CLI do
+  @moduledoc """
+  Handle the command line inputs parsing and the dispatching
+  to the various functions that end up displaying the list of
+  github issues.
+  """
+  @default_count 4
+
+  def run(argv) do
+    parse_args(argv)
+  end
+
+  @doc """
+  `argv` can be -h or --help, which will return :help
+
+  Otherwise, it's a github username, a repository and optionally the number
+  of issues to fetch from github.
+
+  Return a tuple of `{user, project, count}` or :help
+  """
+  def parse_args(argv) do
+    parse = OptionParser.parse(argv, switches: [ help: :boolean],
+                                     aliases:  [ h:    :help   ])
+    case parse do
+      { [ help: true ], _, _ } -> :help
+      { _, [ user, project, count ], _ } -> { user, project, count }
+      { _, [ user, project ], _ } -> { user, project, @default_count }
+      _ -> :help
+    end
+  end
+end
---
+++ test/cli_test.exs
@@ -0,0 +1,19 @@
+defmodule CliTest do
+  use ExUnit.Case
+  doctest Elissues
+
+  import Elissues.CLI, only: [ parse_args: 1 ]
+
+  test ":help returned by option parsing with -h and --help options" do
+    assert parse_args(["-h", "anything"]) == :help
+    assert parse_args(["--help", "something else"]) == :help
+  end
+
+  test "3 values returned if 3 given" do
+    assert parse_args(["zergov", "elissues", 12]) == {"zergov", "elissues", 12}
+  end
+
+  test "count param is defaulted to 4 if only two values is provided" do
+    assert parse_args(["zergov", "elissues"]) == {"zergov", "elissues", 4}
+  end
+end
