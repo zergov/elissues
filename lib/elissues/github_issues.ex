af1bafdd1111e74eb633f437a5d60abffa6b385e
Jonathan Lalande <jonathan.lalande.1@ens.etsmtl.ca>
2019-04-04 00:41:22 -0400

    tests for issues_url

--- lib/elissues/github_issues.ex
+++ lib/elissues/github_issues.ex
@@ -1,4 +1,7 @@
-defmodule Elissues.GithubIssue do
+defmodule Elissues.GithubIssues do
   @user_agent   [ {"User-agent", "Elixir zergov@zergov.dev" } ]
 
+  def issues_url(user, repo) do
+    "https://api.github.com/repos/#{user}/#{repo}/issues"
+  end
 end
--- test/github_issues_test.exs
+++ test/github_issues_test.exs
@@ -2,7 +2,7 @@ defmodule GithubIssuesTest do
   use ExUnit.Case
   doctest Elissues
 
-  import Elissues.GithubIssuesTest, only: [ issues_url: 1 ]
+  import Elissues.GithubIssues, only: [ issues_url: 2 ]
 
   test "issues_url returns a proper github's API url" do
     expected = "https://api.github.com/repos/zergov/elissues/issues"
