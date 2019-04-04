c237625f07d83b5dbff8a1983397035e86844652
Jonathan Lalande <jonathan.lalande.1@ens.etsmtl.ca>
2019-04-04 00:14:18 -0400

    refactor

--- lib/elissues/cli.ex
+++ lib/elissues/cli.ex
@@ -19,13 +19,21 @@ defmodule Elissues.CLI do
   Return a tuple of `{user, project, count}` or :help
   """
   def parse_args(argv) do
-    parse = OptionParser.parse(argv, switches: [ help: :boolean],
-                                     aliases:  [ h:    :help   ])
-    case parse do
-      { [ help: true ], _, _ } -> :help
-      { _, [ user, project, count ], _ } -> { user, project, String.to_integer(count) }
-      { _, [ user, project ], _ } -> { user, project, @default_count }
-      _ -> :help
-    end
+    OptionParser.parse(argv, switches: [ help: :boolean],
+                             aliases:  [ h:    :help   ])
+    |> elem(1)
+    |> args_to_internal_representation()
+  end
+
+  defp args_to_internal_representation([user, project, count]) do
+    { user, project, String.to_integer(count) }
+  end
+
+  defp args_to_internal_representation([user, project]) do
+    { user, project, @default_count }
+  end
+
+  defp args_to_internal_representation(_) do
+    :help
   end
 end
